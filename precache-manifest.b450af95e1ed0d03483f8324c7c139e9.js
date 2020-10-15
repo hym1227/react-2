59cdc945db4cecaa9efc38eabdff72af4f13282f
hym1227 <2407749282@qq.com>
2020-10-15 18:11:01 +0800

    第二次作业

---
+++ precache-manifest.b450af95e1ed0d03483f8324c7c139e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ab9facd24ae823f5a7faa1467c6dd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00ebd6f4eda986a58d28",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "6f55925efa2fdb27aadb",
+    "url": "/static/css/main.04b3727e.chunk.css"
+  },
+  {
+    "revision": "00ebd6f4eda986a58d28",
+    "url": "/static/js/2.c4473dfd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4473dfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f55925efa2fdb27aadb",
+    "url": "/static/js/main.999a4d77.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
